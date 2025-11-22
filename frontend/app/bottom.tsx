6fe5a71988e7272fcea0b726a66970365fcfde81
Anish Kumar <krishkalakar@gmail.com>
2025-11-22 15:06:41 +0530

    added world pages

--- frontend/app/bottom.tsx
+++ frontend/app/bottom.tsx
@@ -2,44 +2,58 @@ import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Link } from "expo-router";
 import { Entypo, Feather, FontAwesome5 } from "@expo/vector-icons";
-import { Href } from "expo-router";
+
 export default function BottomNav() {
   return (
-    <View style={styles.container}>
-      
-      {/* Safe World */}
-      <Link href="/world" asChild>
-        <TouchableOpacity style={styles.iconBox}>
-          <Entypo name="globe" size={26} color="rgba(202, 114, 176, 1)" />
-          <Text style={styles.label}>SafeWorld</Text>
-        </TouchableOpacity>
-      </Link>
+      <View style={styles.container}>
+          {/* Safe World */}
+          <Link href="/world" asChild>
+              <TouchableOpacity style={styles.iconBox}>
+                  <Entypo
+                      name="globe"
+                      size={26}
+                      color="rgba(202, 114, 176, 1)"
+                  />
+                  <Text style={styles.label}>SafeWorld</Text>
+              </TouchableOpacity>
+          </Link>
 
-      {/* Audio Recording */}
-      <Link href="/audiorecording" asChild>
-        <TouchableOpacity style={styles.iconBox}>
-          <Feather name="mic" size={26} color="rgba(202, 114, 176, 1)" />
-          <Text style={styles.label}>Records</Text>
-        </TouchableOpacity>
-      </Link>
+          {/* Audio Recording */}
+          <Link href="/audiorecording" asChild>
+              <TouchableOpacity style={styles.iconBox}>
+                  <Feather
+                      name="mic"
+                      size={26}
+                      color="rgba(202, 114, 176, 1)"
+                  />
+                  <Text style={styles.label}>Records</Text>
+              </TouchableOpacity>
+          </Link>
 
-      {/* Location Sharing */}
-      <Link href="/location" asChild>
-        <TouchableOpacity style={styles.iconBox}>
-          <Entypo name="location-pin" size={26} color="rgba(202, 114, 176, 1)" />
-          <Text style={styles.label}>Location</Text>
-        </TouchableOpacity>
-      </Link>
+          {/* Location Sharing */}
+          <Link href="/location" asChild>
+              <TouchableOpacity style={styles.iconBox}>
+                  <Entypo
+                      name="location-pin"
+                      size={26}
+                      color="rgba(202, 114, 176, 1)"
+                  />
+                  <Text style={styles.label}>Location</Text>
+              </TouchableOpacity>
+          </Link>
 
-      {/* Help */}
-      <Link href="/help" asChild>
-        <TouchableOpacity style={styles.iconBox}>
-          <Feather name="help-circle" size={26} color="rgba(202, 114, 176, 1)" />
-          <Text style={styles.label}>Help</Text>
-        </TouchableOpacity>
-      </Link>
-
-    </View>
+          {/* Help */}
+          <Link href="/help" asChild>
+              <TouchableOpacity style={styles.iconBox}>
+                  <Feather
+                      name="help-circle"
+                      size={26}
+                      color="rgba(202, 114, 176, 1)"
+                  />
+                  <Text style={styles.label}>Help</Text>
+              </TouchableOpacity>
+          </Link>
+      </View>
   );
 }
 
---
+++ frontend/app/world.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const world = () => {
+  return (
+    <View>
+      <Text>world</Text>
+    </View>
+  )
+}
+
+export default world
